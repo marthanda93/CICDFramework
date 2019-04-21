60845dfcbe0fc164400ea13e512cbb39cad2a3af
Anand Kumar <marthanda@Anands-MacBook-Pro.local>
2019-04-22 00:10:29 +0530

    POC: check script

--- src/org/lib/DockerSupport.groovy
+++ src/org/lib/DockerSupport.groovy
@@ -4,36 +4,13 @@ class DockerSupport implements Serializable {
 	private final script
 	Map config
 
-	DockerSupport(script, config = [:]) {
+	static DockerSupport(script, config = [:]) {
 		this.script = script
 		this.config = config
 	}
 
-	void one() {
+	static void one() {
 		script.sh "echo 'Anand Here'"
 		script.println this.script.getClass()
-	}
-
-	void build() {
-		this.script.stage('Building Image') {
-			this.dbuild = this.script.docker.build('maven:sprintboot', '.')
-		}
-	}
-
-	void dbuild() {
-		this.script.stage('Pushing Image') {
-			def document = this.script.libraryResource 'org/epam/docker/maven';
-			this.script.writeFile file: 'Dockerfile', text: document
-			def app = this.script.docker.build("dockerHubID/springboot:latest")
-			return app
-		}
-	}
-
-	void dpush(def app) {
-		this.script.stage('Pushing Image') {
-			this.script.docker.withRegistry('https://registry.hub.docker.com', 'dlogin') {
-				app.push()
-			}
-		}
 	}
 }
