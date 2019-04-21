d0e8d40c67729263b076cf501ed5ae54d5eb0144
Anand Kumar <marthanda@Anands-MacBook-Pro.local>
2019-04-22 00:30:09 +0530

    POC: check script

--- src/org/lib/DockerSupport.groovy
+++ src/org/lib/DockerSupport.groovy
@@ -2,18 +2,20 @@ package org.lib
 
 class DockerSupport implements Serializable {
 	private final script
-	Map config
-	def scripta
+	// Map config
+	// def scripta
 
-	DockerSupport(script, config = [:]) {
-		this.script = script
-		this.config = config
-	}
+	// DockerSupport(script, config = [:]) {
+	// 	this.script = script
+	// 	this.config = config
+	// }
 
-	static void one(scripta) {
-		scripta.sh "echo 'Anand Here'"
-		scripta.println this.script.getClass()
-	}
+	// static void one(scripta) {
+	// 	scripta.sh "echo 'Anand Here'"
+	// 	scripta.println this.script.getClass()
+	// }
+
+  static def mvna(script, args) {
+    script.sh "${script.tool 'Maven'}/bin/mvn -s ${script.env.HOME}/jenkins.xml -o ${args}"
+  }
 }
-
-def docker
