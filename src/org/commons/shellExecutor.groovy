98b0fbb1c65b2db0373f46bee4c34a0b11d0ae9d
Marthanda <devops.marthanda@gmail.com>
2019-04-22 17:24:01 +0530

    POC: check script

--- src/org/commons/shellExecutor.groovy
+++ src/org/commons/shellExecutor.groovy
@@ -3,8 +3,6 @@ package org.commons
 import org.generic.shellRegistry
 
 class shellExecutor implements Serializable, shellRegistry {
-	private final stepExecutor
-
 	shellExecutor(stepExecutor, config = [:]) {
 		this.stepExecutor = stepExecutor
 		this.config = config
