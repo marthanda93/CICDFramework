051ffa9916bb0dd7bc23c9c0a1550aafdc6138a5
Marthanda <devops.marthanda@gmail.com>
2019-04-23 16:15:57 +0530

    april19: script poc

---
+++ src/org/commons/AppTest.groovy
@@ -0,0 +1,18 @@
+package org.commons
+
+import org.generic.IShellRegistry
+
+class AppTest implements Serializable, IAnand {
+	private final stepExe
+	Map config
+
+	AppTest(stepExe, config = [:]) {
+		this.stepExe = stepExe
+		this.config = config
+	}
+
+	@Override
+	Map app() {
+		stepExe.println("--------------------------//Hello")
+	}
+}
