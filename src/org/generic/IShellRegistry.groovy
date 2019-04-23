cd6bbdfc23f46f75462d99bdc63896a8bdecc583
Marthanda <devops.marthanda@gmail.com>
2019-04-23 12:47:48 +0530

    april19: script poc

---
+++ src/org/generic/IShellRegistry.groovy
@@ -0,0 +1,10 @@
+package org.generic
+
+interface IShellRegistry {
+    String bashShell(String command)
+    String batchScript(String command)
+    String powerShellScript(String command)
+
+    String propertyMissing(String name)
+    String methodMissing(String name, Object args)
+}
