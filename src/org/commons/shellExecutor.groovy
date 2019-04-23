b23e2ac68070019ebaef34dd77228890f49e1e94
Marthanda <devops.marthanda@gmail.com>
2019-04-23 12:51:23 +0530

    april19: script poc

--- src/org/commons/shellExecutor.groovy
+++ src/org/commons/shellExecutor.groovy
@@ -4,38 +4,38 @@ import org.generic.IShellRegistry
 import org.generic.MissingObject
 
 class ShellExecutor implements Serializable IShellRegistry MissingObject {
-	private final stepExecutor
+	private final stepExe
 	Map config
 
-	ShellExecutor(stepExecutor, config = [:]) {
-		this.stepExecutor = stepExecutor
+	ShellExecutor(stepExe, config = [:]) {
+		this.stepExe = stepExe
 		this.config = config
 	}
 
 	@Override
 	String bashShell(String command) {
 		try {
-			stepExecutor.sh(script: "${command}", returnStdout: true)
+			stepExe.sh(script: "${command}", returnStdout: true)
 		} catch(e) {
-			stepExecutor.error "ERROR:bashShell: Failed with \n${e.getMessage()}"
+			stepExe.error "ERROR:bashShell: Failed with \n${e.getMessage()}"
 		}
 	}
 
 	@Override
 	String batchScript(String command) {
 		try {
-			stepExecutor.bat(script: "${command}", returnStdout: true)
+			stepExe.bat(script: "${command}", returnStdout: true)
 		} catch(e) {
-			stepExecutor.error "ERROR:bashShell: Failed with \n${e.getMessage()}"
+			stepExe.error "ERROR:bashShell: Failed with \n${e.getMessage()}"
 		}
 	}
 
 	@Override
 	String powerShellScript(String command) {
 		try {
-			stepExecutor.powershell(script: "${command}", returnStdout: true)
+			stepExe.powershell(script: "${command}", returnStdout: true)
 		} catch(e) {
-			stepExecutor.error "ERROR:bashShell: Failed with \n${e.getMessage()}"
+			stepExe.error "ERROR:bashShell: Failed with \n${e.getMessage()}"
 		}
 	}
 }
