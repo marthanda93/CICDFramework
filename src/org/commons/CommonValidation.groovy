b6222c3856e2e689219ece7672a9d14d5470acc3
Marthanda <devops.marthanda@gmail.com>
2019-04-28 02:21:28 +0530

    april19: script poc

--- src/org/commons/CommonValidation.groovy
+++ src/org/commons/CommonValidation.groovy
@@ -3,7 +3,7 @@ package org.commons
 import org.generic.IShellRegistry
 import org.generic.IMissingObject
 
-class CommonValidation implements IMissingObject, Serializable {
+class CommonValidation implements Serializable {
 
 	static boolean stringValidation(String word) {
 		if (word != null && word != "" && word instanceof String && word.size() > 0) {
@@ -11,22 +11,5 @@ class CommonValidation implements IMissingObject, Serializable {
 		} else {
 			return false
 		}
-	}
-
-	@Override
-	String propertyMissing(String name) {
-		"Caught missing property: $name"
-	}
-
-	@Override
-	String methodMissing(String name, Object args) {
-		_steps.println """
-			Possible solutions: 
-			String bashShell(String command)
-			String batchScript(String command)
-			String powerShellScript(String command)
-		"""
-
-		_steps.error "Missing method name is $name"
 	}
 }
