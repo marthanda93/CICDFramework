73200dd89066288aa06e738a4edd8cb228a145d3
Marthanda <devops.marthanda@gmail.com>
2019-05-03 19:54:36 +0530

    may19: script poc

--- vars/ex_git.groovy
+++ vars/ex_git.groovy
@@ -2,5 +2,5 @@ import org.stepRegistry.ContextRegistry
 
 def call(Map pipelineParams) {
     ContextRegistry.registerDefaultContext(this)
-    ContextRegistry.getContext().getGithubExecutor().plainClone(pipelineParams)
+    ContextRegistry.getContext().getGithubExecutor().cloneExecutor(pipelineParams)
 }
