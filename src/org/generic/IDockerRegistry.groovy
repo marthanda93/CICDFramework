128d6a5e152b90b69e9edfc28e8ac0f5859c7e3c
Marthanda <devops.marthanda@gmail.com>
2019-05-03 20:04:34 +0530

    may19: script poc

--- src/org/generic/IDockerRegistry.groovy
+++ src/org/generic/IDockerRegistry.groovy
@@ -1,6 +1,6 @@
 package org.generic
 
 interface IDockerRegistry {
-	Object plainBuild(String path, String image, String registryID = '')
-	Boolean dockerHubPush(Object dObject, String credentialID = '', Boolean latest = false)
+	Object plainBuild(String path, String image, String registryID)
+	Boolean dockerHubPush(Object dObject, String credentialID, Boolean latest)
 }
