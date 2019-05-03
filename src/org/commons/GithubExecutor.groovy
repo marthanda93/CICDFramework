73841198c31b8ed73753024cb19abe84925bb2d9
Marthanda <devops.marthanda@gmail.com>
2019-05-03 19:36:05 +0530

    may19: script poc

--- src/org/commons/GithubExecutor.groovy
+++ src/org/commons/GithubExecutor.groovy
@@ -12,7 +12,7 @@ class GithubExecutor implements IGithubRegistry, IMissingObject, Serializable {
 	}
 
 	@Override
-	Object cloneExecutor(Map appParam && String cloneType = 'plain') {
+	Object cloneExecutor(Map appParam, String cloneType = 'plain') {
 		_steps.cleanWs()
 
 		switch (cloneType.toLowerCase()) {
