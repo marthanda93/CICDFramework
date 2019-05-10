6aac5ad46fdd1dc0d54636542a78bed5ce47018e
Marthanda <devops.marthanda@gmail.com>
2019-05-10 10:39:53 +0530

    may19: code update

--- vars/git.groovy
+++ vars/git.groovy
@@ -1,8 +1,9 @@
 import org.stepRegistry.ContextRegistry
 
 def call(Map pipelineParams, String cloneType = 'plain') {
+	println('-------//1')
     ContextRegistry.registerDefaultContext(this)
-    
+    println('-------//2')
     ContextRegistry.getContext().getGithubExecutor().cloneExecutor(pipelineParams, cloneType)
     ContextRegistry.getContext().getMavenExecutor().extendedBuild()
 }
