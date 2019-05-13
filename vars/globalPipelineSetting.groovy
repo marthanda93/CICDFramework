628eb974fe7f9874254df6ff70ba024071bee518
Marthanda <devops.marthanda@gmail.com>
2019-05-13 22:56:20 +0530

    may19: code update

--- vars/globalPipelineSetting.groovy
+++ vars/globalPipelineSetting.groovy
@@ -54,7 +54,7 @@ import groovy.transform.Field
 ]
 
 @groovy.transform.Field standardization = [
-	templateParameter: '{{ application }}/{{ deploymentStages }}/templateParameter/{{ kubeKind }}',
+	templateParameter: '{{ application }}/{{ deploymentStage }}/templateParameter/{{ kubeKind }}',
 	artifact: '',
 	namespace: 'poc{{ deploymentStage }}',
 	deployment: '',
