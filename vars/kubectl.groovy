e206e3caa478a067e2e9457e71f1ae1cf6a398a4
Marthanda <devops.marthanda@gmail.com>
2019-05-13 21:17:40 +0530

    may19: code update

--- vars/kubectl.groovy
+++ vars/kubectl.groovy
@@ -5,7 +5,11 @@ def call(String action, String className, Object parameter = false) {
 
 	switch (className.toLowerCase()) {
 		case globalPipelineSetting.synonyms.ns:
-			println "_____NameSpace______"
+			try {
+				ContextRegistry.getContext().getK8NameSpaceExecutor()."${action}"()
+			} catch(e) {
+				error "ERROR: Action: ${action} missing!"
+			}
 			break
 		default:
 			error "ERROR:Kubectl: ${className} Undefined!"
