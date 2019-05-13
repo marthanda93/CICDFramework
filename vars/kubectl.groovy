d8ed55e5f3cef429cd6197102b0425bbdb6f3dda
Marthanda <devops.marthanda@gmail.com>
2019-05-13 21:07:06 +0530

    may19: code update

--- vars/kubectl.groovy
+++ vars/kubectl.groovy
@@ -4,7 +4,7 @@ def call(String action, String className, Object parameter = false) {
 	ContextRegistry.registerDefaultContext(this)
 
 	switch (className.toLowerCase()) {
-		case _steps.globalPipelineSetting.synonyms.ns:
+		case globalPipelineSetting.synonyms.ns:
 			println "_____NameSpace______"
 			break
 		default:
