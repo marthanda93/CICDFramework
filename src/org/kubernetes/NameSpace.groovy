ea2e1a2d7e579c7329edb798f617c6ce7b88bd41
Marthanda <devops.marthanda@gmail.com>
2019-05-13 23:36:45 +0530

    may19: code update

--- src/org/kubernetes/NameSpace.groovy
+++ src/org/kubernetes/NameSpace.groovy
@@ -19,7 +19,7 @@ class NameSpace implements IK8NameSpaceRegistry, IMissingObject, Serializable {
 		_steps.println _steps.globalPipelineSetting.standardization.templateParameter.MStringTemplateEngine(k8Param)
 		_steps.println _steps.globalPipelineSetting.standardization.namespace.MStringTemplateEngine(k8Param)
 
-		_steps.println CommonUtilities.executeOnMaster("ls ${_steps.env.JENKINS_HOME}/workspace/${_steps.env.JOB_NAME}@libs")
+		_steps.println CommonUtilities.executeOnMaster("ls ${_steps.env.JENKINS_HOME}/workspace/${_steps.env.JOB_NAME}@libs/${_steps.env.findAll { it.key =~ /^library.(.+).version$/ }.keySet()[0].split('\\.')[1]}")
 
 
 		return true;
