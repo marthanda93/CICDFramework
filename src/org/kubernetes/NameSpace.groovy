6efbbf46cadbe0b7f567957baa7fcc1e10e7bd4d
Marthanda <devops.marthanda@gmail.com>
2019-05-15 00:08:01 +0530

    may19: code update

--- src/org/kubernetes/NameSpace.groovy
+++ src/org/kubernetes/NameSpace.groovy
@@ -17,29 +17,25 @@ class NameSpace implements IK8NameSpaceRegistry, IMissingObject, Serializable {
 		String opsSlaveParameterPath = "${k8Param.opsRepoPath}/${_steps.globalPipelineSetting.standardization.templateParameter.MStringTemplateEngine(k8Param)}/namespace.yaml"
 		String opsMasterParameterPath = "${_steps.env.JENKINS_HOME}/workspace/${_steps.env.JOB_NAME}@libs/${_steps.env.getEnvironment().findAll { it.key =~ /^library.(.+).version$/ }.keySet()[0].split('\\.')[1]}/resources/org/kubernetes"
 // -> validation
-// -> standardization of path
-// -> write yaml from slave to master
-// -> generate
 // -> pull back data from master to slave
 
-if(_steps.fileExists(opsSlaveParameterPath)) {
-	Object path = new File( "${opsMasterParameterPath}/parameter" )
+		if(_steps.fileExists(opsSlaveParameterPath)) {
+			Object path = new File( "${opsMasterParameterPath}/parameter" )
 
-	if(path.exists()) {
-		File file = new File("${opsMasterParameterPath}/parameter/namespace.yaml")
-		file.write "${_steps.readFile(opsSlaveParameterPath)}\n"
-	} else {
-		_steps.println "__path does not exists"
-	}
-}
-
-// _steps.println _steps.readFile "${k8Param.opsRepoPath}/${_steps.globalPipelineSetting.standardization.templateParameter.MStringTemplateEngine(k8Param)}/namespace.yaml")
-
-
+			if(path.exists()) {
+				File file = new File("${opsMasterParameterPath}/parameter/namespace.yaml")
+				file.write "${_steps.readFile(opsSlaveParameterPath)}\n"
+			} else {
+				_steps.println "__path does not exists"
+			}
+		}
 
 		_steps.println CommonUtilities.executeOnMaster("""
-			/usr/bin/j2 -f yaml template/namespace.j2 parameter/namespace.yaml -o output/anand.yaml
+			/usr/bin/j2 -f yaml template/namespace.j2 parameter/namespace.yaml -o output/namespace.yaml
 		""", opsMasterParameterPath)
+
+		_steps.writeYaml file: 'anand.yaml', data: "${new File("${opsMasterParameterPath}/output/namespace.yaml").text}"
+		_steps.println _steps.readYaml file: 'anand.yaml'
 
 		return true;
 	}
