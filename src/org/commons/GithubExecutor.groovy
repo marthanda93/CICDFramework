ee56f9676f1de7c3da7ad455c09777de2b2b18b1
Marthanda <devops.marthanda@gmail.com>
2019-05-16 01:01:40 +0530

    may19: code update

--- src/org/commons/GithubExecutor.groovy
+++ src/org/commons/GithubExecutor.groovy
@@ -45,45 +45,21 @@ class GithubExecutor implements IGithubRegistry, IMissingObject, Serializable {
 	@Override
 	Boolean plainClone(Map appParam) {
 		if(CommonUtilities.gitValidation(appParam)) {
-
-// _steps.checkout([
-// 	$class: 'GitSCM', 
-// 	branches: [[name: "*/${appParam.branch}"]], 
-// 	doGenerateSubmoduleConfigurations: false, 
-// 	extensions: [
-// 		[$class: 'RelativeTargetDirectory', relativeTargetDir: "${appParam.url.split('/')[-1]}"],
-// 		[$class: 'CloneOption', depth: 0, noTags: true, reference: "/opt/${appParam.url.split('/')[-1]}", shallow: false]
-// 	], 
-// 	submoduleCfg: [], 
-// 	userRemoteConfigs: [[
-// 		credentialsId: appParam.credentialsId, 
-// 		refspec: "+refs/heads/${appParam.branch}:refs/remotes/origin/${appParam.branch}",
-// 		url: appParam.url
-// 	]]
-// ])
-
-_steps.checkout changelog: false, poll: false, scm: [
-	$class: 'GitSCM', 
-	branches: [[name: "*/${appParam.branch}"]], 
-	doGenerateSubmoduleConfigurations: false, 
-	extensions: [
-		[$class: 'RelativeTargetDirectory', relativeTargetDir: "${appParam.url.split('/')[-1]}"],
-		[$class: 'CloneOption', depth: 0, noTags: true, reference: "/opt/${appParam.url.split('/')[-1]}", shallow: false]
-	], 
-	submoduleCfg: [], 
-	userRemoteConfigs: [[
-		credentialsId: appParam.credentialsId, 
-		refspec: "+refs/heads/${appParam.branch}:refs/remotes/origin/${appParam.branch}",
-		url: appParam.url
-	]]
-]
-
-// _steps.git(
-// 	changelog: false,
-// 	url: appParam.url, 
-// 	branch: appParam.branch, 
-// 	credentialsId: appParam.credentialsId
-// )
+			_steps.checkout changelog: false, poll: false, scm: [
+				$class: 'GitSCM', 
+				branches: [[name: "*/${appParam.branch}"]], 
+				doGenerateSubmoduleConfigurations: false, 
+				extensions: [
+					[$class: 'RelativeTargetDirectory', relativeTargetDir: "${appParam.url.split('/')[-1]}"],
+					[$class: 'CloneOption', depth: 0, noTags: true, reference: "/opt/${appParam.url.split('/')[-1]}", shallow: false]
+				], 
+				submoduleCfg: [], 
+				userRemoteConfigs: [[
+					credentialsId: appParam.credentialsId, 
+					refspec: "+refs/heads/${appParam.branch}:refs/remotes/origin/${appParam.branch}",
+					url: appParam.url
+				]]
+			]
 		} else {
 			_steps.error "ERROR:Git:plainClone: App Parameter validation failed!\n ${appParam.getClass()} \n ${appParam}"
 		}
