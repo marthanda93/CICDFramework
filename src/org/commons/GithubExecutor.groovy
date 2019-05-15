dad0435c762e3e5980b7434d4ba5ec5f0bc9dff1
Marthanda <devops.marthanda@gmail.com>
2019-05-16 00:00:31 +0530

    may19: code update

--- src/org/commons/GithubExecutor.groovy
+++ src/org/commons/GithubExecutor.groovy
@@ -45,21 +45,38 @@ class GithubExecutor implements IGithubRegistry, IMissingObject, Serializable {
 	@Override
 	Boolean plainClone(Map appParam) {
 		if(CommonUtilities.gitValidation(appParam)) {
-_steps.checkout([
-	$class: 'GitSCM', 
-	branches: [[name: "*/${appParam.branch}"]], 
-	doGenerateSubmoduleConfigurations: false, 
-	extensions: [
-[$class: 'RelativeTargetDirectory', relativeTargetDir: "${appParam.url.split('/')[-1]}"],
-[$class: 'CloneOption', depth: 0, noTags: true, reference: "/opt/${appParam.url.split('/')[-1]}", shallow: false]
-	], 
-	submoduleCfg: [], 
-	userRemoteConfigs: [[
-		credentialsId: appParam.credentialsId, 
-		refspec: "+refs/heads/${appParam.branch}:refs/remotes/origin/${appParam.branch}",
-		url: appParam.url
-	]]
-])
+
+    _steps.checkout([
+        $class: 'GitSCM',
+        branches: [[name: appParam.branch]],
+        doGenerateSubmoduleConfigurations: false,
+        submoduleCfg: [],
+        userRemoteConfigs: [
+            [credentialsId: appParam.credentialsId, url: appParam.url]
+        ]
+    ])
+
+
+
+
+
+
+
+// _steps.checkout([
+// 	$class: 'GitSCM', 
+// 	branches: [[name: "*/${appParam.branch}"]], 
+// 	doGenerateSubmoduleConfigurations: false, 
+// 	extensions: [
+// [$class: 'RelativeTargetDirectory', relativeTargetDir: "${appParam.url.split('/')[-1]}"],
+// [$class: 'CloneOption', depth: 0, noTags: true, reference: "/opt/${appParam.url.split('/')[-1]}", shallow: false]
+// 	], 
+// 	submoduleCfg: [], 
+// 	userRemoteConfigs: [[
+// 		credentialsId: appParam.credentialsId, 
+// 		refspec: "+refs/heads/${appParam.branch}:refs/remotes/origin/${appParam.branch}",
+// 		url: appParam.url
+// 	]]
+// ])
 
 // _steps.checkout changelog: false, poll: false, scm: [
 // 	$class: 'GitSCM', 
