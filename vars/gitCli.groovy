2c55d35c818de8a927194d6bc72a4124067259a7
Marthanda <devops.marthanda@gmail.com>
2019-05-19 00:21:18 +0530

    may19: code update

--- vars/gitCli.groovy
+++ vars/gitCli.groovy
@@ -3,4 +3,6 @@ import org.stepRegistry.ContextRegistry
 def call(Map pipelineParams, String cloneType = 'plain') {
     ContextRegistry.registerDefaultContext(this)
     ContextRegistry.getContext().getGithubExecutor().cloneExecutor(pipelineParams, cloneType)
+
+    globalPipelineSettingTemp.appRepo.url = appRepo
 }
