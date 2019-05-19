5fd2c5bd79bc495e6934c2b00911e095b8b50b7a
Marthanda <devops.marthanda@gmail.com>
2019-05-20 00:05:53 +0530

    may19: code update

--- src/org/commons/GithubExecutor.groovy
+++ src/org/commons/GithubExecutor.groovy
@@ -49,10 +49,10 @@ class GithubExecutor implements IGithubRegistry, IMissingObject, Serializable {
 		if(ContextRegistry.getContext().getShellExecutor().bashShellStatus("[ -d ${_steps.globalPipelineSetting.gitReferencePath}/${appParam.url.split('/')[-1]} ] && exit 0 || exit 1") == 0) {
 			_steps.println "INFO: Reference is available!"
 
-			if(ContextRegistry.getContext().getShellExecutor().bashShellStatus("[ \"\$(ls -A ${_steps.globalPipelineSetting.gitReferencePath}/${appParam.url.split('/')[-1]} | wc -l)\" -gt 0 ] && exit 0 || exit 1") > 0) {
-				_steps.println "ERROR: empty path found!"
-				ContextRegistry.getContext().getShellExecutor().bashShellOutput("rm -rf ${_steps.globalPipelineSetting.gitReferencePath}/${appParam.url.split('/')[-1]}")
-			}
+			// if(ContextRegistry.getContext().getShellExecutor().bashShellStatus("[ \"\$(ls -A ${_steps.globalPipelineSetting.gitReferencePath}/${appParam.url.split('/')[-1]} | wc -l)\" -gt 0 ] && exit 0 || exit 1") > 0) {
+			// 	_steps.println "ERROR: empty path found!"
+			// 	ContextRegistry.getContext().getShellExecutor().bashShellOutput("rm -rf ${_steps.globalPipelineSetting.gitReferencePath}/${appParam.url.split('/')[-1]}")
+			// }
 		} else {
 			_steps.println "WARNING: Reference is not available!"
 		}
