67b54df83c9d9d23609da760ff702931f453a493
Marthanda <devops.marthanda@gmail.com>
2019-05-19 13:02:38 +0530

    may19: code update

--- src/org/commons/GithubExecutor.groovy
+++ src/org/commons/GithubExecutor.groovy
@@ -52,7 +52,7 @@ class GithubExecutor implements IGithubRegistry, IMissingObject, Serializable {
 _steps.println ContextRegistry.getContext().getShellExecutor().bashShellStatus("if [ -d ${_steps.globalPipelineSetting.gitReferencePath}/${appParam.url.split('/')[-1]} ]; then exit 0; else exit 1; fi")
 _steps.println ContextRegistry.getContext().getShellExecutor().bashShellStatus("[ \"\$(ls -A /media 2>&1)\" ] && exit 0 || exit 1")
 
-			if(ContextRegistry.getContext().getShellExecutor().bashShellStatus("[ \"\$(ls -A /media 2>&1)\" ] && exit 0 || exit 1") != 0) {
+			if(ContextRegistry.getContext().getShellExecutor().bashShellStatus("[ \"\$(ls -A /tmp 2>&1)\" ] && exit 0 || exit 1") != 0) {
 				_steps.println "ERROR: empty path found!"
 			}
 		} else {
