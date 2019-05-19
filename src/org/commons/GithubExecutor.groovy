231ee97d0397e124f4667efb4ad45ad8b74a7f93
Marthanda <devops.marthanda@gmail.com>
2019-05-19 12:14:29 +0530

    may19: code update

--- src/org/commons/GithubExecutor.groovy
+++ src/org/commons/GithubExecutor.groovy
@@ -48,7 +48,7 @@ class GithubExecutor implements IGithubRegistry, IMissingObject, Serializable {
 	Boolean plainClone(Map appParam) {
 		if(ContextRegistry.getContext().getShellExecutor().bashShellOutput("if [ -d ${_steps.globalPipelineSetting.gitReferencePath}/${appParam.url.split('/')[-1]} ]; then echo 'true'; else echo 'false'; fi")) {
 			_steps.println "INFO: Reference is available!"
-			if(ContextRegistry.getContext().getShellExecutor().bashShellOutput("[ \"\$(ls -A /tmp)\" ] && echo 'true' || echo 'false'") == true) {
+			if(ContextRegistry.getContext().getShellExecutor().bashShellOutput("[ \"\$(ls -A /tmp)\" ] && echo 'true' || echo 'false'") == false) {
 				_steps.println "ERROR: empty path found!"
 			} else {
 				_steps.println "ERROR: =============//ELSE"
