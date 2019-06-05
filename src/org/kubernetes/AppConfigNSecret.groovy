f560740eddf3673012201066daac98464c74a194
Marthanda <devops.marthanda@gmail.com>
2019-06-05 19:14:18 +0530

    june19: code update

--- src/org/kubernetes/AppConfigNSecret.groovy
+++ src/org/kubernetes/AppConfigNSecret.groovy
@@ -43,7 +43,7 @@ class AppConfigNSecret implements IK8CSRegistry, Serializable {
 
 		files.each { String file ->
 			concatenate.add("${file}".MfnameFromPath()+': |')
-			concatenate.add("    ## ${file}\n")
+			concatenate.add("    ## ${file}")
 			concatenate.addAll(_steps.readFile("${file}").replace('\n', '\n    '))
 		}
 
