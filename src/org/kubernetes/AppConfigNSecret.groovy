c5ac87e58f3ec87eaf4707870d49e1fed8282b2c
Marthanda <devops.marthanda@gmail.com>
2019-06-05 16:52:16 +0530

    june19: code update

--- src/org/kubernetes/AppConfigNSecret.groovy
+++ src/org/kubernetes/AppConfigNSecret.groovy
@@ -20,11 +20,9 @@ class AppConfigNSecret implements IK8CSRegistry, Serializable {
 
 		if(org.generic.CommonUtilities.mapValidation(k8Param)) {
 			if('configPath' in k8Param.keySet().collect()) {
-				_steps.println _steps.findFiles(glob: "${k8Param.scmPath}/${k8Param.configPath}") as List
-				// _steps.println file
-
 				k8Param.configPath.MsubSplit().MsubListjoin().each {
-					files << _steps.findFiles(glob: "${k8Param.scmPath}/petclinic/dev/one/*.properties")
+					_steps.println it
+					files << _steps.findFiles(glob: "${k8Param.scmPath}/petclinic/dev/one/*.properties") as List
 				}
 
 				_steps.println files
